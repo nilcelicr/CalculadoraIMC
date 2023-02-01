2f3b86d3508bcaa7d23a53d8be9e0c0873b1e6c3
nilceli <nilcelicr@hotmail.com>
2023-02-01 17:00:08 +0000

    add ResultActivity.kt

--- app/src/main/java/com/example/meuprimeiroprojeto/ResultActivity.kt
+++ app/src/main/java/com/example/meuprimeiroprojeto/ResultActivity.kt
@@ -22,14 +22,6 @@ class ResultActivity : AppCompatActivity() {
 
             tvResult.text = result.toString()
 
-            /*
-        Menor que 18,5		Abaixo do peso
-        Entre 18,5 a 24,9	Peso normal
-        Entre 25 a 29,9		Sobrepeso
-        Entre 30 a 34,9		Obesidade grau I
-        Entre 35 a 40		Obesidade grau II
-        Maior que 40		Obesidade grau III
-        */
             var classificacao = " "
             classificacao = if (result < 18.5f){
                 "ABAIXO DO PESO"
